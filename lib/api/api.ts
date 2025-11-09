0439932a3480576a9b02aab2193496a7231d432f
Oleksandr Honchar <figules@gmail.com>
2025-11-09 19:48:07 +0100

    fix(api): enable withCredentials for API requests

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api", // ✅ префікс /api залишити
-  withCredentials: false,
+  withCredentials: true,
 });
 
 export type ApiError = AxiosError<{ error: string }>;
