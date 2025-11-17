f7ca8d8972676ddcfc6c82e8835c192bd095c733
Artem <tem4ik20.1@gmail.com>
2025-11-17 18:30:49 +0200

    fix category

--- components/CategoriesSection/PopularCategories.tsx
+++ components/CategoriesSection/PopularCategories.tsx
@@ -1,121 +1,107 @@
 'use client';
-
 import { getCategories } from '@/lib/api/clientApi';
-import { useEffect, useState } from 'react';
+
+import Link from 'next/link';
 import Image from 'next/image';
 import css from './PopularCategories.module.css';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Keyboard, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import { Category } from '@/types/category';
-import Link from 'next/link';
-import { useMediaQuery } from 'react-responsive';
-import Loader from '@/components/Loader/Loader';
+import { useQuery } from '@tanstack/react-query';
 
-const CategoriesList = () => {
-  const [categories, setCategories] = useState<Category[]>(
-    []
-  );
+interface Props {
+  categories: Category[];
+}
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({
-    minWidth: 768,
-    maxWidth: 1439,
+async function PopularCategories() {
+  const {
+    data = [],
+    error,
+    isLoading,
+    isError,
+  } = useQuery<Category[]>({
+    queryKey: ['category'],
+    queryFn: () => getCategories(1, 10),
   });
-  const isDesktop = useMediaQuery({ minWidth: 1440 });
-  const defaultVisibleCount = isDesktop ? 6 : 4;
-  const [visibleCount, setVisibleCount] = useState(
-    defaultVisibleCount
-  );
-  const [page, setPage] = useState(1);
-  const [more, setMore] = useState(true);
-  const perPage = 10;
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      setLoading(true);
-      try {
-        const data = await getCategories(page, perPage);
-        setCategories(prev => {
-          const newItems = data.filter(
-            item => !prev.some(cat => cat._id === item._id)
-          );
-          return [...prev, ...newItems];
-        });
-        if (data.length < perPage) setMore(false);
-      } catch (err) {
-        setError((err as Error).message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchCategories();
-  }, [page]);
-
-  useEffect(() => {
-    setVisibleCount(isDesktop ? 6 : 4);
-  }, [isDesktop, isTablet, isMobile]);
-
-  const handleLoadMore = () => {
-    if (isMobile) setVisibleCount(prev => prev + 1);
-    else if (isTablet) setVisibleCount(prev => prev + 2);
-    else if (isDesktop) setVisibleCount(prev => prev + 3);
-  };
-
-  if (loading) return <Loader />;
-
-  if (error) return <p className={css.error}>{error}</p>;
-
-  const visibleCategories = categories.slice(
-    0,
-    visibleCount
-  );
-  const hasMore = visibleCount < categories.length;
-
+  const categories = Array.isArray(data) ? data : [];
   return (
-    <section className={css.categoriesSection}>
+    <section
+      className={css.categoriesSection}
+      id="PopularCategories"
+    >
       <div className={css.categories}>
-        <h2 className={css.title}>Категорії</h2>
+        <h2 className={css.title}>Популярні категорії</h2>
+        <div className={css.buttonLink}>
+          <Link href="/categories" className={css.button}>
+            Всі категорії
+          </Link>
+        </div>
       </div>
-      <div>
+
+      <div className={css.categoriesContainer}>
         <ul className={css.CategoriesList}>
-          {visibleCategories.map(category => (
-            <li
-              key={category._id}
-              className={css.CategoriesItem}
-            >
-              <Link
-                href={`/categories/${category._id}`}
-                className={css.CategoriesLink}
-              >
-                <div className={css.imageWrapper}>
-                  <Image
-                    src={category.image}
-                    alt={category.name}
-                    width={416}
-                    height={277}
-                    className={css.categoriesIMG}
-                  />
-                </div>
-                <p className={css.categoriesText}>
-                  {category.name}
-                </p>
-              </Link>
-            </li>
-          ))}
+          <Swiper
+            modules={[Navigation, Keyboard, A11y]}
+            spaceBetween={32}
+            slidesPerView={1}
+            navigation={{
+              prevEl: `.${css.prevButton}`,
+              nextEl: `.${css.nextButton}`,
+              disabledClass: css.disabledButton,
+            }}
+            keyboard={{ enabled: true }}
+            breakpoints={{
+              768: { slidesPerView: 2, spaceBetween: 32 },
+              1440: {
+                slidesPerView: 3,
+                spaceBetween: 32,
+              },
+            }}
+            className={css.categoriesSwiper}
+          >
+            {categories.map(category => (
+              <SwiperSlide key={category._id}>
+                <Link
+                  href={`/categories/${category._id}`}
+                  className={css.CategoriesLink}
+                >
+                  <div className={css.imageWrapper}>
+                    <Image
+                      src={category.image}
+                      alt={category.name}
+                      width={416}
+                      height={277}
+                      className={css.categoriesIMG}
+                    />
+                  </div>
+                  <p className={css.categoriesText}>
+                    {category.name}
+                  </p>
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </ul>
       </div>
 
-      {hasMore && (
-        <button
-          className={css.button}
-          onClick={handleLoadMore}
-        >
-          Показати більше
+      <div className={css.swiperButtons}>
+        <button className={css.prevButton}>
+          <svg>
+            <use href="/sprite.svg#icon-arrow-back" />
+          </svg>
         </button>
-      )}
+        <button className={css.nextButton}>
+          <svg>
+            <use href="/sprite.svg#icon-arrow-forward" />
+          </svg>
+        </button>
+      </div>
     </section>
   );
-};
+}
 
-export default CategoriesList;
+export default PopularCategories;
